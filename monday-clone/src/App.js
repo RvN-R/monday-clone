46beb10ec82fa178959bb25c03a50df13256058c
RvN-R <rvannairn@googlemail.com>
2022-04-13 12:17:05 +0000

    installed react-router-dom and added routes to app.js

--- monday-clone/src/App.js
+++ monday-clone/src/App.js
@@ -1,10 +1,22 @@
- 
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Nav from './components/nav'
+import Dashboard from './pages/dashboard'
+import TicketPage from './pages/ticketpage'
 const App = () => {
   return (
     <div className="app">
-    
+      <Nav/>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Dashboard/>}/>
+          <Route path='/ticket' element={<TicketPage/>}/>
+          <Route path='/ticket/:id' element={<TicketPage editMode={true}/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
 
 export default App;
+
+// 16:49
